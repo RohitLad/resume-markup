95b55d74e2b7e06715f058eb68949c3c988f7823
Rohit Lad <rohit.lad.sae@gmail.com>
2025-10-12 22:30:36 +0200

    profile resource hidden from navbar

--- app/Filament/Dashboard/Resources/Profiles/ProfileResource.php
+++ app/Filament/Dashboard/Resources/Profiles/ProfileResource.php
@@ -20,6 +20,28 @@ class ProfileResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
+    protected static bool $shouldRegisterNavigation = false;
+
+    public static function canViewAny(): bool
+    {
+        return false;
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
+
+    public static function canEdit($record): bool
+    {
+        return false;
+    }
+
+    public static function canDelete($record): bool
+    {
+        return false;
+    }
+
     public static function form(Schema $schema): Schema
     {
         return ProfileForm::configure($schema);
