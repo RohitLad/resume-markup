8bb7479eaa008f450698918c2c8d5cbcf2567ed2
Rohit Lad <rohit.lad.sae@gmail.com>
2025-10-26 23:58:54 +0100

    knowledgebase added to profile

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -9,10 +9,19 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Profile extends Model
 {
     use HasUuids;
+
     protected $keyType = 'string';
+
     public $incrementing = false;
-    protected $fillable = ['user_id', 'data'];
-    protected $casts = ['data'=>'array'];
+
+    protected $fillable = ['user_id', 'data', 'knowledgebase', 'knowledgebase_updated_at'];
+
+    protected $casts = [
+        'data' => 'array',
+        'knowledgebase' => 'array',
+        'knowledgebase_updated_at' => 'datetime',
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
